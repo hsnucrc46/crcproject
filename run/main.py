1170e6d5a355c96c01d2ca937639e68d3ed0fe93
Oliver Tzeng <olivertzeng@proton.me>
2024-04-12 15:42:27 +0800

    utf8 4 windows bruh

--- run/main.py
+++ run/main.py
@@ -1,6 +1,6 @@
 from rich import print
 from rich.markdown import Markdown
 
-with open("src/INTRO.md") as readme:
+with open("src/INTRO.md", encoding="utf8") as readme:
     markdown = Markdown(readme.read())
 print(markdown)
