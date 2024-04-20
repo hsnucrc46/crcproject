eef7cf63a2e3613f3264931fb2f62c97d0bcbdaa
Oliver Tzeng <olivertzeng@proton.me>
2024-04-20 09:20:05 +0800

    📝 docs:(README) nerdfont sucks on windows bruh

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # CRC Project 師大附中電算社最後成品
 
-* 未完成，敬請期待！
+* 已完成，敬請期待！
 
 ## 預定成品形式
-* `Python `
+* `Python 🐍`
 * GUI Pygame
 
 - 資料夾形式
--- run/spaceship/src/lib.py
+++ run/spaceship/src/lib.py
@@ -18,7 +18,7 @@ height = get_monitors()[0].height
 width = get_monitors()[0].width
 max = 2000
 min = 250
-max_time = 100
+max_time = 120
 step = -10
 time = 0
 
