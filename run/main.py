844bdfb9f6402f89e0fa881bb0c6ca28ac845654
Oliver Tzeng <olivertzeng@proton.me>
2024-04-20 11:01:08 +0800

    decomment and fixed path

--- run/main.py
+++ run/main.py
@@ -1,14 +1,14 @@
 import os
-# import time
-#
-# from rich import print
-# from rich.markdown import Markdown
-#
-# with open("src/INTRO.md", encoding="utf8") as readme:
-#     markdown = Markdown(readme.read())
-# print(markdown)
-#
-# time.sleep(10)
-# os.system("python underlying/main.py")
-# os.system("python spaceship/main.py")
+import time
+
+from rich import print
+from rich.markdown import Markdown
+
+with open("src/INTRO.md", encoding="utf8") as readme:
+    markdown = Markdown(readme.read())
+print(markdown)
+
+time.sleep(10)
+os.system("python underlying/main.py")
+os.system("python spaceship/main.py")
 os.system("python 123/123.py")
